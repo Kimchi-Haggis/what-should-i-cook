9117ba8703068bba77d1e02d464047731882e6e3
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-10-31 15:02:46 +0000

    [Design] layout prototype

--- components/common/Section/Hero.tsx
+++ components/common/Section/Hero.tsx
@@ -4,19 +4,21 @@ type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font h-screen">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">What should I cook?
           </h1>
-          <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+          <div className="mb-8 leading-relaxed">
+            <p>Are you feeling bored to cook same things everyday?</p>
+            <p>Do you have so many left overs in your fridge?</p>
+            <p>Have no ideas where to start for cooking?</p>
+          </div>
           <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+          <img className="object-cover object-center rounded" alt="hero" src="https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*" />
         </div>
       </div>
     </section>
