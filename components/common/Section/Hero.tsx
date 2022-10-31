3b631696a7902acacfaaad916e252d09a4b1d8c1
wave64 <hippiengypsy02@gmail.com>
2022-10-31 17:51:29 +0000

    [Feat] implement search box in hero section

--- components/common/Section/Hero.tsx
+++ components/common/Section/Hero.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SearchBar from './../searchBar/SearchBar';
 
 type Props = {}
 
 const Hero = (props: Props) => {
   return (
-    <section className="text-gray-600 body-font h-screen">
+    <section className="text-gray-600 body-font">
+      <SearchBar className='mx-4 md:mx-10 mt-10' placeHolder='e.g., tomato, garlic, pepper'/>
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">What should I cook?
