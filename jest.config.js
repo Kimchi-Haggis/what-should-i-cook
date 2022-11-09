fdc9b32c990f487db3774098570d104dd7bc1ac1
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-11-09 00:26:24 +0000

    [Setup] jest alias import

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@public/(.*)$': '<rootDir>/public/$1',
+    '^@libs/(.*)$': '<rootDir>/libs/$1',
+    '^@constants/(.*)$': '<rootDir>/constants/$1',
+    '^@pages/(.*)$': '<rootDir>/pages/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@layouts/(.*)$': '<rootDir>/layouts/$1',
+  },
+};
